cd9f4f7d8c98fed320aa8b68cb95f7bf84c9a6ca
Victor kariuki <vikgachewa2hoptmail.com>
2023-08-30 13:16:47 +0300

    cicd

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+const appName =
+    process.env.NODE_ENV === "production"
+        ? "topjoint_api-production"
+        : "topjoint_api-development";
+
+module.exports = {
+    apps: [
+        {
+            name: appName,
+            script: "npm run start",
+            instances: 4,
+            exec_mode: "cluster",
+            env_production: {
+                NODE_ENV: process.env.NODE_ENV,
+            },
+            env_development: {
+                NODE_ENV: process.env.NODE_ENV,
+            },
+            watch: false,
+            "log_date_format": "YYYY-MM-DD HH:mm Z"
+        },
+    ],
+};
